d48c0fa9c5b6991aa987f5610994187992876d28
neo masilo <neolawrencemasilo@gmail.com>
2024-03-14 07:58:25 -0700

    solves the lockboxes problem

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -17,7 +17,7 @@ def canUnlockAll(boxes):
             node = queue.pop(0)
 
             for n in boxes[node]:
-                if 0 <= key < (len(boxes)) and not visited[n]:
+                if 0 <= n < (len(boxes)) and not visited[n]:
                     visited[n] = True
                     queue.append(n)
         return all(visited)
