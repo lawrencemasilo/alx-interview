367c66678a97d42fb56f5193e7b3b7ce16f0668e
Neo <neolawrencemasilo@gmail.com>
2024-05-02 02:06:07 -0700

    returns minimum number of coins needed to reach the total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,15 +15,8 @@ def makeChange(coins, total):
     min_num[0] = 0
 
     for coin in coins:
-        if coin <= total:
-            min_num[coin] = 1
-
-    max_coin_value = max(coins)
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                min_num[i] = min(min_num[i], min_num[i - coin] + 1)
-
+        for i in range(coin, total + 1):
+            min_num[i] = min(min_num[i], min_num[i - coin] + 1)
     if min_num[total] == float('inf'):
         return -1
     return min_num[total]
